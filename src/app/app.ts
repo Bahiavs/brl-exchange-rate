62795d3f94c8f9342b747297c7a698258f363951
Vinicius Bahia <vini.bahiarj@gmail.com>
2026-01-30 19:34:56 -0300

    icons, mocks, style, texts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [MatInputModule, MatButtonModule],
+  imports: [MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: 'app.html',
   styleUrl: 'app.css',
 })
 export class App {
+  protected readonly last30DaysExchange = [
+    {
+      date: '09/03/2022',
+      open: 5.0666,
+      close: 5.0038,
+      high: 5.0680,
+      low: 4.9836,
+      closeFiff: -1.15
+    },
+    {
+      date: '07/03/2022',
+      open: 5.0666,
+      close: 5.0038,
+      high: 5.0680,
+      low: 4.9836,
+      closeFiff: 1.15
+    },
+  ]
+  protected readonly isExpanded = signal(true)
+
+  protected toggleExpansion() {
+    this.isExpanded.update(isExpanded => !isExpanded)
+  }
 }
