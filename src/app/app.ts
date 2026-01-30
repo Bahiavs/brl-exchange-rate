1c51b1a7140d4647867533f6bc63524d9396a4fa
Vinicius Bahia <vini.bahiarj@gmail.com>
2026-01-30 14:37:59 -0300

    update logo files, favicon and title 10min

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   template: `
-    <h1>Hello, {{ title() }}</h1>
-
-    <router-outlet />
+    Hello, BRL Exchange Rate!
   `,
   styles: [],
 })
 export class App {
-  protected readonly title = signal('brl-exchange-rate');
 }
